57bb057267c22d411c94f5996b3b3b84a2d98423
aleciodalpra <aleciodalpra@gmail.com>
2017-06-22 13:56:35 -0300

    Add files via upload

---
+++ calculadora_despesas.py
@@ -0,0 +1,14 @@
+#Calculadora de despesas
+
+print ('BALANCO DE DESPESAS')
+alecio = input('Quanto gastou Alecio? ')
+joana = input('Quanto gastou Joana? ')
+total = alecio + joana
+
+print ('Total de gastos: R$ %.') % total
+
+media = total/2
+
+print ('Media de gasto por pessoa: R$ %s.') % media
+print ('.....................')
+print ('ACERTO DE CONTAS')
