51776576920a0e288b41a2daedc6da696c29af32
Unknown <aleciodalpra@gmail.com>
2017-07-02 16:12:16 -0300

    exes_0107

--- Exerc 2/e15_salarioMensal.py
+++ Exerc 2/e15_salarioMensal.py
@@ -1,7 +1,9 @@
 # https://wiki.python.org.br/EstruturaSequencial
 
+#-*- coding: utf-8 -*-
+
 n2 = float(input('Informe o valor da hora: '))
-n1 = float(input('Informe a quantidade horas trabalhadas por mes: '))
+n1 = float(input('Informe a quantidade horas trabalhadas por mês: '))
 
 salBruto = n1 * n2
 ir = salBruto * 0.11
@@ -9,9 +11,9 @@ inss = salBruto * 0.08
 sindicato = salBruto * 0.05
 salLiq = salBruto - ir - inss - sindicato
 print('')
-print('+ Salario Bruto: R$ %.2f' % salBruto)
+print('+ Salário Bruto: R$ %.2f' % salBruto)
 print('- IR: R$ %.2f' % ir)
 print('- INSS: R$ %.2f' % inss)
 print('- Sindicato: R$ %.2f' % sindicato)
-print('= Salario Liquido: R$ %.2f' % salLiq)
+print('= Salário Liquido: R$ %.2f' % salLiq)
 print('')
